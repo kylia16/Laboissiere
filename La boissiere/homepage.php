efde5d792ecf717ea0e0f779b4c3a502920ac6bf
kylia16 <moussingaamandine@gmail.com>
2025-09-17 22:01:08 +0100

    Update homepage.php

--- La boissiere/homepage.php
+++ La boissiere/homepage.php
@@ -1,4 +1,7 @@
- <meta charset="UTF-8">
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>La Boissière - Système de Gestion</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
